95752ba091fe0f08842e1e7e0921182311082079
egb2000 <hecenbe@163.com>
2019-03-22 21:40:34 +0800

    测试提交

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -15,6 +15,7 @@ namespace Tetris
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
+
         }
     }
 }
